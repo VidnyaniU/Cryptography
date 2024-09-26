966c536373d80fef0345fca767ab80cd5ead71f9
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-09-26 13:51:22 +0530

    Updated ElGamal encryption-decryption

--- elGamal.cpp
+++ elGamal.cpp
@@ -10,22 +10,28 @@ Vec<ZZ_p> DLP::elGamal_encryption(ZZ_p g, long x, long m)
     C1_C2.SetLength(2);
 
     // choose y from {0,1,2,..,p-1}
-    ZZ_p y = random_ZZ_p();
+    ZZ_p y = conv<ZZ_p>("6");
+    // ZZ_p y = random_ZZ_p();
+    // long p = conv<long>(ZZ_p::modulus());
+    // long y = RandomBnd(p);
+
+    cout << "Y :: " << y << endl;
     // compute s = h^y
     ZZ_p h = dlp(g, x);
     ZZ_p s = dlp(h, conv<long>(y));
 
     // compute C1 = g^y
-    ZZ_p C1_C2[0] = dlp(g, conv<long>(y));
+    C1_C2[0] = dlp(g, conv<long>(y));
 
     // compute C2 = m.s
-    ZZ_p C1_C2[1] = m * s;
+    C1_C2[1] = m * s;
     return C1_C2;
 }
 
-long elGamal_decryption(Vec<ZZ_p> C1_C2, long x)
+long DLP::elGamal_decryption(Vec<ZZ_p> C1_C2, long x)
 {
 
     long ans = conv<long>(C1_C2[1]) * conv<long>(inv(power(C1_C2[0], x)));
+    // cout << "inv(power(C1_C2[0], x)) :: " << conv<long>(power(C1_C2[0], x)) << endl;
     return ans;
 }
--- main.cpp
+++ main.cpp
@@ -6,10 +6,12 @@ using namespace NTL;
 int main()
 {
     // string _p = "11";
-    ZZ p = conv<ZZ>("11"); // all instances of the ZZ_p will have the fixed p value now
+    ZZ p = conv<ZZ>("17"); // all instances of the ZZ_p will have the fixed p value now
     ZZ_p::init(p);
-    ZZ_p g = conv<ZZ_p>("2");
+    ZZ_p g = conv<ZZ_p>("3");
     DLP obj1;
+
+    /*
 
     long a = 3, b = 2; // a and b are secret keys of Alice and Bob
     ZZ_p Alice = obj1.diffie_hellman_encryption(g, a);
@@ -23,6 +25,19 @@ int main()
 
     if (Alice_dash == Bob_dash)
         cout << "Decryption Successful!" << endl;
+
+*/
+
+    // Alice sends message m to Bob
+    long m = 9;
+    long x = 5;
+
+    Vec<ZZ_p> encrypted_message = obj1.elGamal_encryption(g, x, m);
+    cout << "\nC1 :: " << encrypted_message[0] << "\nC2 :: " << encrypted_message[1] << endl;
+
+    // Decryption of message at Bob's end
+    long decrypted_message = obj1.elGamal_decryption(encrypted_message, x);
+    cout << "Decrypted message :: " << decrypted_message << endl;
 
     return 0;
 }
