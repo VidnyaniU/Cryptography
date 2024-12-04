3efba1aa7b3ea8a457cebdbd929ce5516743b979
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-12-04 09:24:58 +0530

    Added pollard rho on ecdlp

---
+++ pollardRhoOnEcdlp.cpp
@@ -0,0 +1,17 @@
+#include <NTL/ZZ.h>
+#include <NTL/EllipticCurve.h>
+#include <iostream>
+#include <random>
+
+using namespace NTL;
+using namespace std;
+
+// Function to generate random numbers in a range
+ZZ randomZZ(const ZZ& limit) {
+    static random_device rd;
+    static mt19937_64 gen(rd());
+    uniform_int_distribution<long long> dis(0, to_long(limit) - 1);
+    return ZZ(dis(gen));
+}
+
+
