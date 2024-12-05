3ac1402f32cfca3ab57e3e884a882c4d9ebace0d
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-12-05 08:59:07 +0530

    Updated pollards rho on ecdlp

--- pollardRhoOnEcdlp.cpp
+++ pollardRhoOnEcdlp.cpp
@@ -14,4 +14,9 @@ ZZ randomZZ(const ZZ& limit) {
     return ZZ(dis(gen));
 }
 
+// Struct for an elliptic curve point with coefficients
+struct ECPoint {
+    ZZ x, y;
+    ZZ a, b; // Coefficients of the linear combination Q = aP + bG
+};
 
