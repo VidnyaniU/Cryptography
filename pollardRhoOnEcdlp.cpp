478d49fb386aaa1479bb5aa320d183bb12635acd
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2024-12-08 09:53:54 +0530

    Updated pollard rho

--- pollardRhoOnEcdlp.cpp
+++ pollardRhoOnEcdlp.cpp
@@ -7,7 +7,8 @@ using namespace NTL;
 using namespace std;
 
 // Function to generate random numbers in a range
-ZZ randomZZ(const ZZ& limit) {
+ZZ randomZZ(const ZZ &limit)
+{
     static random_device rd;
     static mt19937_64 gen(rd());
     uniform_int_distribution<long long> dis(0, to_long(limit) - 1);
@@ -15,18 +16,21 @@ ZZ randomZZ(const ZZ& limit) {
 }
 
 // Struct for an elliptic curve point with coefficients
-struct ECPoint {
+struct ECPoint
+{
     ZZ x, y;
     ZZ a, b; // Coefficients of the linear combination Q = aP + bG
 };
 
 // pseudo-random partitioning function
-int partition(const ZZ& x, int numPartitions) {
+int partition(const ZZ &x, int numPartitions)
+{
     return to_long(x % numPartitions);
 }
 
 // Pollard Rho Algorithm for ECDLP
-ZZ pollardsRhoECDLP(const EllipticCurve& E, const ECPoint& G, const ECPoint& Q, const ZZ& n) {
+ZZ pollardsRhoECDLP(const EllipticCurve &E, const ECPoint &G, const ECPoint &Q, const ZZ &n)
+{
     int numPartitions = 3; // Number of partitions
     ECPoint Tortoise = G, Hare = G;
 
@@ -37,36 +41,45 @@ ZZ pollardsRhoECDLP(const EllipticCurve& E, const ECPoint& G, const ECPoint& Q, 
     Hare.b = randomZZ(n);
 
     // Random walks using the partition function
-    auto step = [&](ECPoint& P) {
+    auto step = [&](ECPoint &P)
+    {
         int region = partition(P.x, numPartitions);
-        if (region == 0) {
+        if (region == 0)
+        {
             // Update as P + G
             P = E.add(P, G);
             P.a = (P.a + 1) % n;
-        } else if (region == 1) {
+        }
+        else if (region == 1)
+        {
             // Double the point
             P = E.dbl(P);
             P.a = (2 * P.a) % n;
             P.b = (2 * P.b) % n;
-        } else {
+        }
+        else
+        {
             // Update as P + Q
             P = E.add(P, Q);
             P.b = (P.b + 1) % n;
         }
     };
 
-    while (true) {
+    while (true)
+    {
         // Tortoise once, Hare twice
         step(Tortoise);
         step(Hare);
         step(Hare);
 
-        if (Tortoise.x == Hare.x && Tortoise.y == Hare.y) {
+        if (Tortoise.x == Hare.x && Tortoise.y == Hare.y)
+        {
             // Found a collision
             ZZ deltaA = (Hare.a - Tortoise.a) % n;
             ZZ deltaB = (Tortoise.b - Hare.b) % n;
 
-            if (deltaB == 0) {
+            if (deltaB == 0)
+            {
                 throw runtime_error("Collision with zero difference in coefficients!");
             }
 
@@ -75,7 +88,13 @@ ZZ pollardsRhoECDLP(const EllipticCurve& E, const ECPoint& G, const ECPoint& Q, 
             InvMod(k, deltaB, n); // Inverse of deltaB mod n
             k = (deltaA * k) % n;
 
-            return (k + n) % n; 
+            return (k + n) % n;
         }
     }
+}
+
+int main()
+{
+
+    return 0;
 }
